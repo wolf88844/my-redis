819b88a420401401cc463e64958f7630ff060256
wolf88844 <wolf88840@163.com>
2025-01-02 08:56:22 +0800

    feat(client): 实现 Redis 客户端基本功能
    - 添加 Get 和 Set 命令的处理逻辑
    - 实现客户端与 Redis 服务器的连接和通信
    - 使用 tokio 实现异步任务处理
    - 通过 mpsc 和 oneshot 通道进行任务分发和结果接收

---
+++ src/bin/echo-server-copy.rs
@@ -0,0 +1,28 @@
+use tokio::{io::{AsyncReadExt, AsyncWriteExt}, net::TcpListener};
+
+
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6142").await.unwrap();
+
+    loop{
+        let (mut socket,_) = listener.accept().await.unwrap();
+        tokio::spawn(async move{
+            let mut buf = [0; 1024];
+            loop{
+                match socket.read(&mut buf).await{
+                    Ok(0) => return,
+                    Ok(n)=>{
+                        if socket.write_all(&buf[..n]).await.is_err(){
+                            return;
+                        }
+                    }
+                    Err(_) => {
+                        return;
+                    },
+                }
+            }
+        });
+    }
+}
